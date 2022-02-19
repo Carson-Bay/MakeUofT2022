1d712bc5e640be58306782ed7c37e1cd0bac261d
Carson Bay <64244261+Carson-Bay@users.noreply.github.com>
2022-02-19 21:08:26 +0000

    initialize pump output pins

--- hardware/pump.py
+++ hardware/pump.py
@@ -37,6 +37,8 @@ class Pump:
     # Setup GPIO
     self.pump_gpio = pump_gpio
     self.button_gpio = button_gpio
+    GPIO.setup(self.pump_pin, GPIO.OUT)
+    GPIO.output(self.pump_pin, GPIO.LOW)
     # All time values in milliseconds
     self.last_on = get_time_millis()
     self.last_off = get_time_millis()
